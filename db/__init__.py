419ad34760dcc80e3652643b6712bc851d6a7ab7
Viktor Vlasov <viktorvlasovsiberian@gmail.com>
2019-01-11 23:24:27 +0200

    [db][sqlalchemy] automap metadata helper function

--- db/__init__.py
+++ db/__init__.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.automap import automap_base as ab
-from sqlalchemy import create_engine as ce
+from sqlalchemy import create_engine as ce, MetaData
 from sqlalchemy.orm import Session
 
 def create_engine(url):
@@ -17,6 +17,15 @@ def automap_base(engine):
     Base = ab()
     Base.prepare(engine, reflect=True)
     return Base
+
+def automap_meta(engine):
+    """Creates SQLAlchemy MetaData reflected with engine
+    Parameters:
+    engine - SQLAlchemy engine
+    """
+    meta = MetaData()
+    meta.reflect(engine)
+    return meta
 
 def session(engine):
     """Creates SQLAlchemy session
